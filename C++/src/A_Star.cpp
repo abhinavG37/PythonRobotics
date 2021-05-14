e75b9bc03cafa80ef0355054544975fd0c98973f
Abhinav Gupta <abhinavgupta1998@gmail.com>
2021-05-15 04:24:23 +0530

    Visualization errors unrectified

--- C++/src/A_Star.cpp
+++ C++/src/A_Star.cpp
@@ -8,12 +8,19 @@
 
 #include <iostream>
 #include <cmath>
+#include <utility>
 #include <vector>
 #include <map>
 #include <string>
-#include <eigen3/Eigen/Dense>
 #include <sciplot/sciplot.hpp>
+#include<opencv2/core.hpp>
+#include<opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 using namespace std;
+
+
+
 
 class A_Star_planner{
 public:
@@ -22,19 +29,20 @@ public:
         grid = grid_;
         resolution = resolution_;
         rr = rr_;
-        ox = ox_;
-        oy = oy_;
+        ox = std::move(ox_);
+        oy = std::move(oy_);
         this->min_x=0.0, this->min_y = 0.0;
         this->max_x=0.0, this->max_y = 0.0;
         this->x_width = 0.0; this->y_width = 0.0;
         this->motion = this->get_motion_model();
+
         this->obmap =this->calc_obmap(ox, oy);
     } //CONSTRUCTOR FOR CLASS A*_Planner
 
     ////////////////////////CLASS NODE//////////////////////////////////////////////////
      class Node{
         public:
-            Node(float x, float y, float cost, int parent_index, Node* p_node_=NULL){
+            Node(float x, float y, float cost, int parent_index, Node* p_node_=nullptr){
                 this->x = x;
                 this->y = y;
                 this->cost = cost;
@@ -42,14 +50,14 @@ public:
                 parent_node = p_node_;
             } //DEFAULT CONSTRUCTOR TO INITIALIZE NODE BASED ON PARENT NODE
             
-            string str_maker(){
+            [[nodiscard]] string str_maker() const{
                 return to_string(this->x)+ "," + to_string(this->y) + "," + to_string(
                 this->cost) + "," +to_string(this->parent_index);
             }
         //Data members of subclass Node
         float x, y, cost;
-        float  parent_index;   
-        Node* parent_node;
+        float  parent_index;
+        [[maybe_unused]] Node* parent_node;
     };
     //////////////////////////////////////////END CLASS NODE//////////////////////////////////
  
@@ -79,29 +87,50 @@ public:
 // {  TAKES A pointer to NODE object}     //calc_grid_index(Node* node)
          open_set.insert(pair<int, Node*>(this->calc_grid_index(startNode), startNode));
          int count =0;
-        while(true){
+        cv::Mat bg(this->resolution*this->x_width,
+                 this->resolution*this->y_width,
+                 CV_8UC3,
+                 cv::Scalar(255,255,255));
+
+
+      while(true){
             count += 1;
             printf("%d\n", count);
-            if(open_set.size() == 0){
+            if(open_set.empty()){
                 printf("OPEN SET EMPTY");
                 break;
             }
             
             //LINE 81: Needs a lambda function equivalent ---C++ functor---- to capture max cost Node in open_set  
             map<int, Node*>::iterator it;
-            float temp_min_cost = 0; 
+
             int c_id= 0; //grid index of current node used as key in open_set 
             
      
             //BLOCK FUNCTIONING NOT PROPER
             for(it = open_set.begin(); it != open_set.end(); it++){
-                temp_min_cost = (it->second->cost)+this->calc_heuristic(goalNode, it->second);
+                auto temp_min_cost = (it->second->cost)+this->calc_heuristic
+                                                    (goalNode, it->second);
                 if( (it->second->cost)+this->calc_heuristic(goalNode, it->second) <= temp_min_cost){
-                    temp_min_cost =(it->second->cost)+this->calc_heuristic(goalNode, it->second);
                     c_id = it->first;
                 }
             } //find key corresponding to a temp_min_cost
             Node* current = open_set.at(c_id); //ERROR
+
+
+          cv::rectangle(bg,
+                        cv::Point(startNode->x*this->resolution+1,
+                                  startNode->y*this->resolution+1),
+                        cv::Point((startNode->x+1)*this->resolution,
+                                  (startNode->y+1)*this->resolution),
+                        cv::Scalar(255, 0, 0), -1); //Create RED SQUARE ON
+                                                    // START NODE
+          cv::rectangle(bg,
+                        cv::Point(goalNode->x*this->resolution+1,
+                                  goalNode->y*this->resolution+1),
+                        cv::Point((goalNode->x+1)*this->resolution,
+                                  (goalNode->y+1)*this->resolution),
+                        cv::Scalar(0, 0, 255), -1);
 
             if(current->x  == goalNode->x && current->y == goalNode->y){
                 printf("-------------Goal Found-------------");
@@ -115,26 +144,38 @@ public:
      
         
             /////////continue here from line 110
-            for (unsigned int i =0; i<motion.size(); i++){
-                Node* node = new Node(current->x+motion[i][0],
-                                                        current->y+motion[i][1],
-                                                        current->cost+motion[i][2], 
+            for (auto & i : motion){
+                Node* node = new Node(current->x+i[0],
+                                                        current->y+i[1],
+                                                        current->cost+i[2],
                                                         c_id);
                 int n_id = this->calc_grid_index(node);
 
                 //Verification of new node
-                //Node not out of bounds or collding with obstacle
+                //Node not out of bounds or colliding with obstacle
                 if(!this->verify_node(node)){ continue;}
             
-                //Node exists on already traversed node then do nothin
+                //Node exists on already traversed node then do nothing
                 if(closed_set.count(n_id)>0){ continue; }
                
                 if(open_set.count(n_id)==0){ open_set.insert(pair<int, Node*>(n_id, node)); }
                 else{
                     if(open_set.at(n_id)->cost > node->cost){open_set.insert(pair<int, Node*>(n_id, node)); }
                 }
+              cv::rectangle(bg,
+                            cv::Point(node->x*this->resolution+1,
+                                        node->y*this->resolution+1),
+                            cv::Point((node->x+1)*this->resolution, (node->y+1)*this->resolution),
+                            cv::Scalar(0, 255, 0));
+
+              cv::imshow("astar", bg);
+              int k = cv::waitKey(5);
+              if((k & 0xFF) == 27) {
+                cv::destroyAllWindows();
+              }
             }
-        }
+
+          }
         return this->calc_final_path(goalNode, closed_set);
     }
 
@@ -154,9 +195,13 @@ public:
         this->x_width = round((this->max_x - this->min_x) / this->resolution);
         this->y_width = round((this->max_y - this->min_y) / this->resolution);
         printf("x_width: %f", this->x_width);
-        printf("y_width: %f", this->y_width);       
-        
-        vector<vector<int>> temp_obmap(this->y_width, vector<int>(this->x_width, 0));
+        printf("y_width: %f", this->y_width);
+      cv::Mat img(this->resolution*this->x_width,
+                  this->resolution*this->y_width,
+                  CV_8UC3,
+                  cv::Scalar(100,100,100));
+        vector<vector<int>> temp_obmap((int)this->y_width, vector<int>
+            ((int)this->x_width, 0));
         for(int  i =0; i<this->x_width; i++){
             int x = this->calc_grid_pos(i, this->min_x);
             for(int j = 0; j<this->y_width; j++){
@@ -164,7 +209,15 @@ public:
                 for(unsigned int k = 0; k<ox.size(); k++){
                     float d = sqrt(pow((ox[k]-x), 2)+pow((oy[k]-y), 2));
                     if(d<=this->rr){
-                        temp_obmap[i][j] = 1; 
+                        temp_obmap[i][j] = 1;
+                        cv::rectangle(this->bg, cv::Point(i*this->resolution+1,
+                                                     j*this->resolution+1),
+                                      cv::Point((i+1)*this->resolution, (j+1)*this->resolution),cv::Scalar(0,0,0), 1);
+                        cv::imshow("astar", img);
+                        int k = cv::waitKey(5);
+                        if((k & 0xFF) == 27) {
+                          cv::destroyAllWindows();
+                        }
                         break;
                     }
                 }
@@ -191,20 +244,20 @@ public:
   return {rx, ry};
     }
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
-   inline  float calc_grid_pos(int index,float min_pos){
+   [[nodiscard]] inline  float calc_grid_pos(int index,float min_pos) const{
        return  index * this->resolution+min_pos;
    }
       ///////////////////////////////////////////////////////////////////////////////////////////////////// 
-    inline int calc_xy_index( float position, float min_pos){
+    [[nodiscard]] inline int calc_xy_index( float position, float min_pos) const{
         return round((position - min_pos) / this->resolution);
     }
    /////////////////////////////////////////////////////////////////////////////////////////////////////
-    inline int calc_grid_index(Node* node){ 
+    inline int calc_grid_index(Node* node) const{
         return ((node->y-this->min_y)*this->x_width)+(node->x-this->min_x);
     }//calculates delX+delY between min pos of grid and current pos of node
    ///////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-    float calc_heuristic(Node* n1, Node* n2){
+    static float calc_heuristic(Node* n1, Node* n2){
         float w = 1.0;
         float dist = w*sqrt(pow((n1->x-n2->x),2) + pow( (n1->y-n2->y),2));
         return dist;    
@@ -224,7 +277,7 @@ public:
         return true;
     }
 ///////////////////////////////////////////////////////////////////////////////////////////
-    vector<vector<float>> get_motion_model(){
+    static vector<vector<float>> get_motion_model(){
                                                                
          vector<vector<float>> motion = {//{dx   dy    cost}
                                                                 {1.0, 0.0, 1.0},
@@ -246,6 +299,7 @@ public:
     float resolution, rr, min_x, min_y, max_x, max_y, x_width, y_width;
     vector<vector<float>> motion;
     vector<float> ox, oy;
+    cv::Mat bg;
 //////////////////END OF A_STAR_PLANNER CLASS/////////////////
 };
 
@@ -297,7 +351,8 @@ int main(int argc, char* argv[]){
         oy_.push_back(60.0 - i);
     }
 
-    //Calling constructor for A_Star_planner Class 
+    //Calling constructor for A_Star_planner Class
+    cv::namedWindow("astar", cv::WINDOW_FULLSCREEN);
     A_Star_planner planner_obj(grid_rows, grid_cols, grid_resolution, ox_, oy_, robot_radius);
     //Calling planner function
     vector<vector<float>> rxry = planner_obj.planner(sx, sy, gx,gy);
